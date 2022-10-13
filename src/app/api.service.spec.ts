42c19297adb1c10443e2bcd0e01e648d33640a0d
fbrv01 <fbucholc@gmail.com>
2022-10-13 17:30:02 +0200

    added tests

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,15 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
+    });
+    injector = getTestBed();
+    service = injector.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#getAlldogs', () => {
+    it('should return an Observable<dogs[]>', () => {
+      const dummyDogs = [{ dog1: 'John' }, { dog2: 'Doe' }];
+
+      service.getAllDogsList().subscribe((dogs) => {
+        expect(dogs).toEqual(dummyDogs);
+      });
+
+      const req = httpMock.expectOne(`https://dog.ceo/api/breeds/list/all`);
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyDogs);
+    });
+  });
+
+  describe('#getImageByBreed', () => {
+    it('should return an Observable<dogs[]>', () => {
+      const dummyImage = [{ img_name: 'name1' }];
+
+      service.getImageByBreed('test').subscribe((img) => {
+        expect(img).toEqual(dummyImage);
+      });
+
+      const req = httpMock.expectOne(
+        `https://dog.ceo/api/breed/test/images/random`
+      );
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyImage);
+    });
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AppComponent],
+      providers: [HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -22,12 +24,43 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('PsIntagram');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'PsIntagram app is running!'
-    );
+  describe('selectDog', () => {
+    it('should set the correct wikipedia url, when event is given', () => {
+      const event = { target: { value: 'TTTTT' } };
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectDog(event);
+      expect(app.wikipediaUrl).toEqual('https://en.wikipedia.org/wiki/TTTTT');
+    });
+
+    it('should return set wikipedia entry to null, when given event is undefined', () => {
+      const event = { target: { value: undefined } };
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectDog(event);
+      expect(app.wikipediaUrl).toBeNull();
+    });
+  });
+
+  describe('getKeys', () => {
+    it('should return the correctKeys for Breeds', () => {
+      const sampleBreed = {
+        name: 'Breed1',
+        size: 'big',
+        hasChildren: false,
+      };
+      const expectedResult = ['name', 'size', 'hasChildren'];
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.getKeys(sampleBreed)).toEqual(expectedResult);
+    });
+
+    it('should return an empty array when no breeds are give', () => {
+      const sampleBreed = null;
+      const expectedResult: any[] = [];
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.getKeys(sampleBreed)).toEqual(expectedResult);
+    });
   });
 });
